e40e3a8325e4d2794f64f95d3bf451052579b463
BT_1N3_33 <ljaoutakas8934@bths.edu>
2018-10-04 14:44:09 -0400

    10/4/18 2:43

--- src/LjaoutakasLib.java
+++ src/LjaoutakasLib.java
@@ -1,5 +1,23 @@
 public class LjaoutakasLib
 {
+    public static Boolean isFibonnaci(int n)
+    {
+        Boolean output = false;
+        int num1 = 0;
+        int num2 = 1;
+        int num3 = 0;
+        while(num3<n)
+        {
+            num3 = num2 + num1;
+            num1 = num2;
+            num2 = num3;
+            if (num3 == n)
+            {
+                output = true;
+            }
+        }
+        return output;
+    }
     public static String quadSolver(double a, double b, double c)
     {
         if (Math.pow(b,2)-4*a*c > 0 )
@@ -12,5 +30,17 @@ public class LjaoutakasLib
         {
             return ("The roots are imaginary");
         }
+    }
+    public static String vigCipher(String msg, String key)
+    {
+        String enc = "";
+        msg = msg.toLowerCase();
+        key = key.toLowerCase();
+
+        for (int i = 0; i < msg.length(); i++)
+        {
+
+        }
+        return enc;
     }
 }
--- src/TestFile.java
+++ src/TestFile.java
@@ -1,7 +1,20 @@
 public class TestFile
 {
+
+
     public static void main(String args[])
     {
+        System.out.println("isFibonnaci(3):");
+        System.out.println(LjaoutakasLib.isFibonnaci(3));
+        System.out.println("isFibonnaci(4):");
+        System.out.println(LjaoutakasLib.isFibonnaci(4));
+            //isFibonacci of a
+        System.out.println("quadSolver(x^2+x+20):");
         System.out.println(LjaoutakasLib.quadSolver(1,1,20));
+            // An equation with imaginary roots
+        System.out.println("quadSolver(x^2+x-20):");
+        System.out.println(LjaoutakasLib.quadSolver(1,1,-20));
+            // An equation with real roots
     }
+
 }
